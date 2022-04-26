334d0ed93ae956384406aafc9a47c5b0a84a99ba
Josue <josuepereztorr@gmail.com>
2022-04-25 20:27:08 -0500

    -NLog.Web.AspNetCore 5.0.0-rc2 -nlog.config

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ namespace NorthwindDatabaseApplication
 {
     class Program
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Logger.Info("Program Started");
         }
     }
 }
